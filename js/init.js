f962b898569a1a54ef423d9fcef12c50108bc648
Juuso Lehtinen <juuso.lehtinen@affecto.com>
2015-06-05 15:01:56 +0300

    load features to map

--- js/init.js
+++ js/init.js
@@ -1,6 +1,7 @@
+var map, featureLayer;
+
 window.onload = function() {
 var xhr = new XMLHttpRequest();
-var map;
 
 xhr.open('GET', 'http://avoindata.maanmittauslaitos.fi/mapcache/wmts?service=wmts&request=getcapabilities&version=1.0.0', true);
 
@@ -29,6 +30,10 @@ function init() {
     var parser = new ol.format.WMTSCapabilities();
     var capabilities = parser.read(xhr.responseXML);
 
+    featureLayer = new ol.layer.Vector({
+        source: new ol.source.Vector()
+    });
+
     map = new ol.Map({
         layers: [
             /*new ol.layer.Tile({
@@ -54,12 +59,32 @@ function init() {
                   params: {'LAYERS': 'lipas_kaikki_kohteet'},
                   serverType: 'geoserver'
                 })
-              })
+              }),
+            featureLayer
         ],
         target: 'map',
         view: view
     });
 }
 
-
 };
+
+function getNearestSports (argument) {
+    $.get('http://', function (data) {
+        var source = featureLayer.getSource();
+
+        // Remove old features from map
+        source.clear();
+
+        for (var i = 0; i < data.length; i++) {
+            var x = data[i].x;
+            var y = data[i].y;
+
+            var feature = new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.transform([x, y], 'EPSG:4326', 'EPSG:3067'))
+            });
+
+            source.addFeature(feature);
+        }
+    });
+}
